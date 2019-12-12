d9c5ecb6d5074d06ccda002939effd413dccb930
kee cha <kee_cha18@yahoo.com>
2019-12-11 21:14:12 -0600

    init

---
+++ binary_searchtree/LinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_searchtree
+{
+    public class LinkedList
+    {
+        Node root;
+        Node current;
+
+        public void InsertAtEnd(int data)
+        {
+            Node node = new Node(data);
+            if (root == null)
+            {
+                root = node;
+                return;
+            }
+            else
+            {
+                current = node;
+            }
+            current = root;
+            while (true)
+            {
+                if (data < current.data)
+                {
+                    if (current.left == null)
+                    {
+                        current.left = node;
+                        break;
+                    }
+                    else if (current.left != null)
+                    {
+                        current = current.left;
+                    }
+                }
+                else if (data >= current.data)
+                {
+                    if (current.right == null)
+                    {
+                        current.right = node;
+                        break;
+                    }
+                    else if (current.right != null)
+                    {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ binary_searchtree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_searchtree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList LL = new LinkedList();
+            LL.InsertAtEnd(100);
+            LL.InsertAtEnd(55);
+            LL.InsertAtEnd(77);
+            LL.InsertAtEnd(35);
+            LL.InsertAtEnd(120);
+            LL.InsertAtEnd(1);
+            LL.InsertAtEnd(82);
+            LL.InsertAtEnd(145);
+            LL.InsertAtEnd(115);
+            Console.WriteLine();
+        }
+    }
+}
